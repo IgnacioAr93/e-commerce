fdf2d0f341719260d18c9d14d0c15b8edf880642
IgnacioAr93 <arayaignacio2493@gmail.com>
2017-12-22 22:17:10 -0300

    Actions Products and Categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
 	has_many :product_category
 	has_many :product, through: :product_category
+	validates :title, presence: true
+	validates :title, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+resources :categories do
+	resources :products
+end
+
+
 devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 get '/user/auth/facebook/callback'
 root 'welcome#index'
